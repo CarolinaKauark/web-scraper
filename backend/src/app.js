5b7d0e293838fb448147a14279a099ed9d943503
Ckauark <ckauark@gmail.com>
2023-05-24 13:05:23 -0300

    fix: cors policy third try

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', '*');
-    app.use(cors());
+    // app.use(cors());
     next();
 });
 
