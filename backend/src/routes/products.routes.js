e4b438431597de20fbe5e50139e36e4bf7e43e0c
Ckauark <ckauark@gmail.com>
2023-05-24 11:15:19 -0300

    feat: adding a get / route to see if the server is running as expected

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -4,6 +4,7 @@ const productsController = require('../controllers/products.controller');
 
 const router = express.Router();
 
+router.get('/', (req, res) => res.status(200).json('OK') )
 router.post('/', postMiddleware, productsController.getProducts);
 
 module.exports = router;
