8d73a57717cb1cefd5d50388d32f05dadaec7e1f
Ckauark <ckauark@gmail.com>
2023-05-24 13:13:37 -0300

    fix: deleting the timeout request

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,21 +4,14 @@ const errorMiddleware = require('./middlewares/errorMiddleware');
 const router = require('./routes/products.routes');
 
 const app = express();
-// app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', '*');
-    // app.use(cors());
+    app.use(cors());
     next();
-});
-
-//bigger time out to allow time to the webscraper
-app.use(function(req, res, next) {
-  res.setTimeout(300);
-  next();
 });
 
 app.use(router);
