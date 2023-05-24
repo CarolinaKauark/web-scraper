fba66bad3af6850f6703b0a7d743750b69b4143f
Ckauark <ckauark@gmail.com>
2023-05-24 12:05:49 -0300

    feat: preparing for deploy

--- backend/src/models/Connection.js
+++ backend/src/models/Connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/WebScraper';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/WebScraper';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
-    || MONGO_DB_URL,
 ) => mongoose.connect(mongoDatabaseURI);
 
 module.exports = connectToDatabase;
